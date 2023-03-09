b046dfd97e3ea08d6bc6ad8ee4a91152cf4c69ee
ZeeiDev <zeeideveloper19@gmail.com>
2023-03-09 11:15:28 +0700

    option bitrate

--- livestream.php
+++ livestream.php
@@ -13,43 +13,45 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 	// build ffmpeg command
 	$ffmpeg_cmd = 'ffmpeg -re -i "'.$video_path.'"';
 	$ffmpeg_cmd .= ' -c:v '.$video_codec.' -c:a '.$audio_codec.' -preset '.$preset.' -b:v '.$bitrate.'k';
-	$ffmpeg_cmd .= ' -s '.$resolution.' -r '.$framerate.' -f flv "rtmp://a.rtmp.youtube.com/live2/'.$stream
-// start ffmpeg process
-$descriptorspec = array(
-	0 => array('pipe', 'r'), // stdin is a pipe that the child will read from
-	1 => array('pipe', 'w'), // stdout is a pipe that the child will write to
-	2 => array('pipe', 'w') // stderr is a pipe that the child will write to
-);
-$process = proc_open($ffmpeg_cmd, $descriptorspec, $pipes);
-
-if (is_resource($process)) {
-	// send success response to client
-	$response = array(
-		'status' => 'success'
+	$ffmpeg_cmd .= ' -s '.$resolution.' -r '.$framerate.' -f flv "rtmp://a.rtmp.youtube.com/live2/'.$stream_key;
+	// start ffmpeg process
+	$descriptorspec = array(
+		0 => array('pipe', 'r'), // stdin is a pipe that the child will read from
+		1 => array('pipe', 'w'), // stdout is a pipe that the child will write to
+		2 => array('pipe', 'w') // stderr is a pipe that the child will write to
 	);
-	echo json_encode($response);
+	$process = proc_open($ffmpeg_cmd, $descriptorspec, $pipes);
 
-	// wait for ffmpeg process to finish
-	while (($line = fgets($pipes[1])) !== false) {
-		// output ffmpeg log to console
-		echo $line;
+	if (is_resource($process)) {
+		// send success response to client
+		$response = array(
+			'status' => 'success'
+		);
+		echo json_encode($response);
 
-		// flush output buffer to prevent server timeout
-		ob_flush();
-		flush();
+		// wait for ffmpeg process to finish
+		while (($line = fgets($pipes[1])) !== false) {
+			// output ffmpeg log to console
+			echo $line;
+
+			// flush output buffer to prevent server timeout
+			ob_flush();
+			flush();
+		}
+
+		// close pipes and process
+		fclose($pipes[0]);
+		fclose($pipes[1]);
+		fclose($pipes[2]);
+		proc_close($process);
+	} else {
+		// send error response to client
+		$response = array(
+			'status' => 'error',
+			'message' => 'Gagal Memulai Live Streaming'
+		);
+		echo json_encode($response);
 	}
 
-	// close pipes and process
-	fclose($pipes[0]);
-	fclose($pipes[1]);
-	fclose($pipes[2]);
-	proc_close($process);
-} else {
-	// send error response to client
-	$response = array(
-		'status' => 'error',
-		'message' => 'Gagal Memulai Live Streaming'
-	);
-	echo json_encode($response);
 }
 ?>
